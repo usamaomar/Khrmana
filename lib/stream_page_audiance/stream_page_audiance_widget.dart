3a18cfc58dc35bc9d7c0a6a3f8218aa8b4be4c0a
usamaKhateeb <usamaomarsoftware@gmail.com>
2023-10-03 17:22:55 +0300

    s

--- lib/stream_page_audiance/stream_page_audiance_widget.dart
+++ lib/stream_page_audiance/stream_page_audiance_widget.dart
@@ -1,3 +1,5 @@
+import 'package:zego_uikit_prebuilt_live_streaming/zego_uikit_prebuilt_live_streaming.dart';
+
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -49,7 +51,17 @@ class _StreamPageAudianceWidgetState extends State<StreamPageAudianceWidget> {
           top: true,
           child: Column(
             mainAxisSize: MainAxisSize.max,
-            children: [],
+            children: [
+              ZegoUIKitPrebuiltLiveStreaming(
+                appID: 2073713405,
+                appSign:
+                '1fead54d1af0497cd9bb0e104a860e5e0361468d9f21b47b066028807ccd2edc' /*input your AppSign*/,
+                userID: "uok",
+                userName: "usama",
+                liveID: '123',
+                config: ZegoUIKitPrebuiltLiveStreamingConfig.audience(),
+              )
+            ],
           ),
         ),
       ),
